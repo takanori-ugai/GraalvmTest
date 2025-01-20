792539f7c0f0d93e986456fc445da96af0a54a27
Takanori Ugai <ugai@fujitsu.com>
2025-01-20 11:23:15 +0900

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+    application
+    id("org.graalvm.buildtools.native") version "0.10.4"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.fasterxml.jackson.core:jackson-core:2.18.2")
+implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+implementation("com.fasterxml.jackson.core:jackson-annotations:2.18.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("demo.Fortune")
+//    mainClass.set("demo.HelloKt")
+}
+
+graalvmNative {
+	/*
+	binaries {
+		named("main") {
+			buildArgs.add("--initialize-at-build-time=kotlin")
+		}
+	}
+	*/
+    binaries.all {
+        resources.autodetect()
+    }
+}
+
+graalvmNative {
+    toolchainDetection.set(true)
+}
